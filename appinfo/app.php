59dc06ee1fa32d338683568062ba1fb57f7d9f28
Fallon Turner <fcturner@users.noreply.github.com>
2015-07-24 14:45:03 +0200

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -5,12 +5,15 @@ use OCP\AppFramework\App;
 $app = new App('passwords');
 $container = $app->getContainer();
 
-$l = OCP\Util::getL10N('passwords');
+$l = \OCP\Util::getL10N('passwords');
+
+\OCP\App::registerAdmin('passwords', 'templates/admin.settings');
+\OCP\App::registerPersonal('passwords', 'templates/personal.settings');
 
 $urlGenerator = $container->query('OCP\IURLGenerator');
 $l10n = $container->query('OCP\IL10N');
 
-OCP\App::addNavigationEntry(array(
+\OCP\App::addNavigationEntry(array(
 		'id' => 'passwords',
 		'order' => 9999,
 		'href' => $urlGenerator->linkToRoute('passwords.page.index'),
