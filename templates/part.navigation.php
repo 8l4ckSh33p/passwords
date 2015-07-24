6ba6a2b11f1ccc6d085bcb51db5b4f9ea1632eb7
Fallon Turner <fcturner@users.noreply.github.com>
2015-07-24 15:26:33 +0200

    Update part.navigation.php

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -18,6 +18,7 @@
 
 <div id="add_password_div">
 	<h3><?php p($l->t("Add new password")); ?></h3>
+	<input type="text" id="new_address" placeholder="<?php p($l->t("Full URL (optional)")); ?>" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
 	<input type="text" id="new_website" placeholder="<?php p($l->t("site.com or Name Inc.")); ?>" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
 	<input type="text" id="new_username" placeholder="<?php p($l->t("Login name or e-mail")); ?>" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
 	<input type="text" id="new_password" placeholder="<?php p($l->t("Password")); ?>" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
