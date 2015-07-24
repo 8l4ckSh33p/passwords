95a2f5afffe1aea80f35cca123b9d336f4177330
Fallon Turner <fcturner@users.noreply.github.com>
2015-07-24 14:55:40 +0200

    Update passwordmapper.php

--- db/passwordmapper.php
+++ db/passwordmapper.php
@@ -6,18 +6,27 @@ use OCP\AppFramework\Db\Mapper;
 
 class PasswordMapper extends Mapper {
 
-    public function __construct(IDb $db) {
-        parent::__construct($db, 'passwords', '\OCA\Passwords\Db\Password');
-    }
+	public function __construct(IDb $db) {
+		parent::__construct($db, 'passwords', '\OCA\Passwords\Db\Password');
+	}
 
-    public function find($id, $userId) {
-        $sql = 'SELECT * FROM *PREFIX*passwords WHERE id = ? AND user_id = ?';
-        return $this->findEntity($sql, [$id, $userId]);
-    }
+	public function find($id, $userId) {
+		$sql = 'SELECT * FROM *PREFIX*passwords WHERE id = ? AND user_id = ?';
+		return $this->findEntity($sql, [$id, $userId]);
+	}
 
-    public function findAll($userId) {
-        $sql = 'SELECT * FROM *PREFIX*passwords WHERE user_id = ?';
-        return $this->findEntities($sql, [$userId]);
-    }
+	public function findAll($userId) {
+		// OLD: $sql = 'SELECT * FROM *PREFIX*passwords WHERE user_id = ?';
+		// Now, search for shared too on 'oc_share':
+		$sql = 'SELECT * FROM *PREFIX*passwords WHERE user_id = ? OR id IN (SELECT item_source FROM *PREFIX*share WHERE share_with = ? AND item_type = "password")';
+		return $this->findEntities($sql, [$userId, $userId]);
+	}
+
+	// public function sharedWith($id) {
+	// 	// @id - id of passwords
+	// 	// @uid - uid of user shared with
+	// 	$sql = 'SELECT item_source AS "id", share_with AS "uid" FROM *PREFIX*share WHERE item_source = ? AND item_type = "password"';
+	// 	return $this->findEntities($sql, [$id]);
+	// }
 
 }
