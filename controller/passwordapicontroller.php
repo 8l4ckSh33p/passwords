566f9d75f1d667536be204ec83d2c0dc15e86e9d
Fallon Turner <fcturner@users.noreply.github.com>
2015-09-15 15:55:57 +0200

    Update passwordapicontroller.php

--- controller/passwordapicontroller.php
+++ controller/passwordapicontroller.php
@@ -9,78 +9,78 @@ use OCA\Passwords\Service\PasswordService;
 
 class PasswordApiController extends ApiController {
 
-    private $service;
-    private $userId;
+	private $service;
+	private $userId;
 
-    use Errors;
+	use Errors;
 
-    public function __construct($AppName, IRequest $request,
-                                PasswordService $service, $UserId){
-        parent::__construct($AppName, $request);
-        $this->service = $service;
-        $this->userId = $UserId;
-    }
+	public function __construct($AppName, IRequest $request,
+								PasswordService $service, $UserId){
+		parent::__construct($AppName, $request);
+		$this->service = $service;
+		$this->userId = $UserId;
+	}
 
-    /**
-     * @CORS
-     * @NoCSRFRequired
-     * @NoAdminRequired
-     */
-    public function index() {
-        return new DataResponse($this->service->findAll($this->userId));
-    }
+	/**
+	 * @CORS
+	 * @NoCSRFRequired
+	 * @NoAdminRequired
+	 */
+	public function index() {
+		return new DataResponse($this->service->findAll($this->userId));
+	}
 
-    /**
-     * @CORS
-     * @NoCSRFRequired
-     * @NoAdminRequired
-     *
-     * @param int $id
-     */
-    public function show($id) {
-        return $this->handleNotFound(function () use ($id) {
-            return $this->service->find($id, $this->userId);
-        });
-    }
+	/**
+	 * @CORS
+	 * @NoCSRFRequired
+	 * @NoAdminRequired
+	 *
+	 * @param int $id
+	 */
+	public function show($id) {
+		return $this->handleNotFound(function () use ($id) {
+			return $this->service->find($id, $this->userId);
+		});
+	}
 
-    /**
-     * @CORS
-     * @NoCSRFRequired
-     * @NoAdminRequired
-     *
-     * @param string $loginname
-     * @param string $website
-     */
-    public function create($loginname, $website, $address, $pass, $notes) {
-        return $this->service->create($loginname, $website, $address, $pass, $notes, $this->userId);
-    }
+	/**
+	 * @CORS
+	 * @NoCSRFRequired
+	 * @NoAdminRequired
+	 *
+	 * @param string $loginname
+	 * @param string $website
+	 */
+	public function create($loginname, $website, $address, $pass, $notes, $deleted) {
+		return $this->service->create($loginname, $website, $address, $pass, $notes, $deleted, $this->userId);
+	}
 
-    /**
-     * @CORS
-     * @NoCSRFRequired
-     * @NoAdminRequired
-     *
-     * @param int $id
-     * @param string $loginname
-     * @param string $website
-     */
-    public function update($id, $loginname, $website, $address, $pass, $notes) {
-        return $this->handleNotFound(function () use ($id, $loginname, $website, $address, $pass, $notes) {
-            return $this->service->update($id, $loginname, $website, $address, $pass, $notes, $this->userId);
-        });
-    }
+	/**
+	 * @CORS
+	 * @NoCSRFRequired
+	 * @NoAdminRequired
+	 *
+	 * @param int $id
+	 * @param string $loginname
+	 * @param string $website
+	 */
+	public function update($id, $loginname, $website, $address, $pass, $notes, $deleted) {
+		return $this->handleNotFound(function () use ($id, $loginname, $website, $address, $pass, $notes, $deleted) {
+			return $this->service->update($id, $loginname, $website, $address, $pass, $notes, $deleted, $this->userId);
+		});
+	}
 
-    /**
-     * @CORS
-     * @NoCSRFRequired
-     * @NoAdminRequired
-     *
-     * @param int $id
-     */
-    public function destroy($id) {
-        return $this->handleNotFound(function () use ($id) {
-            return $this->service->delete($id, $this->userId);
-        });
-    }
+	/**
+	 * @CORS
+	 * @NoCSRFRequired
+	 * @NoAdminRequired
+	 *
+	 * @param int $id
+	 */
+	public function destroy($id) {
+		return $this->handleNotFound(function () use ($id) {
+			return $this->service->delete($id, $this->userId);
+		});
+	}
 
 }
