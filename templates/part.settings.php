c4f6ec411e87e45d442a973f52deb77829b11334
Fallon Turner <fcturner@users.noreply.github.com>
2015-10-10 14:08:09 +0200

    Update part.settings.php

--- templates/part.settings.php
+++ templates/part.settings.php
@@ -1,4 +1,28 @@
 <div id="app-settings" active-table="active">
+	<div id="CSVtableDIV">
+		<textarea id="CSVcontent"></textarea>
+		<textarea id="CSVcolumnCount"></textarea>
+		<h2></h2>
+		<h3><?php p($l->t('Select options')); ?>:</h3>
+		<div id="CSVbuttons">
+			<button id="CSVheadersBtn"><?php p($l->t('File contains headers')); ?></button>
+			<button id="CSVquotationmarksBtn"><?php p($l->t('Values are seperated by quotation marks')); ?></button>
+			<button id="CSVescapeslashBtn"><?php p($l->t('Characters %s and %s need to be escaped', array('\\', '"'))); ?></button>
+			<button id="CSVsplit_rnBtn"><?php p($l->t('Lines are split on')); ?> \r\n</button>
+		</div>
+		<br><br><br>
+		<h3 id="CSVpreviewTitle"><?php p($l->t('Preview')); ?> :</h3>
+		<div id="CSVtableScroll">
+			<table id="CSVtable">
+				<!-- TABLE WILL BE POPULATED WITH JS -->
+			</table>
+		</div>
+		<button id="selectAll"><?php p($l->t('Select all')); ?></button>
+		<button id="selectNone"><?php p($l->t('Select none')); ?></button>
+		<div id="CSVerrorfield"></div>
+		<button id="importCancel"><?php p($l->t('Cancel')); ?></button>
+		<button id="importStart"><?php p($l->t('Import')); ?></button>
+	</div>
 	<div id="app-settings-header">
 		<button class="settings-button"
 				data-apps-slide-toggle="#app-settings-content"
@@ -13,24 +37,6 @@
 		</div>
 		<div id="app-settings-csv">
 			<h3><?php p($l->t('Import CSV File')); ?></h3>
-			<p><?php p($l->t('Choose a format and a select a file from disk')); ?>.</p>
-			<label><input type="radio" id="owncloud-csv" name="csv_type" checked><?php p($theme->getName() . ' ' . $l->t('Passwords')); ?></label><br>
-			<label><input type="radio" id="keepass-csv" name="csv_type">KeePass CSV (1.x)</label><br>
-			<label><input type="radio" id="onepassword-csv" name="csv_type">1Password CSV</label><br>
-			<label><input type="radio" id="lastpass-csv" name="csv_type">LastPass CSV</label><br>
-			<label><input type="radio" id="splash-csv" name="csv_type">SplashID vID</label><br>
-			<label><input type="radio" id="other-csv" name="csv_type"><?php p($l->t('Other')); ?> CSV</label>
-			<div id="other-csv-list">
-				<br>
-				<p><strong><?php p($l->t("Fill in the column numbers")); ?>:</strong></p>
-				<p id="subtitle"><?php p($l->t("Use 'x' to exclude a field")); ?></p>
-				<div class="other-csv-list"><label><?php p($l->t('Website or company')); ?><input type="text" id="website-csv" value="1"></label></div>
-				<div class="other-csv-list"><label><?php p($l->t('Login name')); ?><input type="text" id="login-csv" value="2"></label></div>
-				<div class="other-csv-list"><label><?php p($l->t('Password')); ?><input type="text" id="password-csv" value="3"></label></div>
-				<div class="other-csv-list"><label><?php p($l->t('Full URL (optional)')); ?><input type="text" id="url-csv" value="4"></label></div>
-				<div class="other-csv-list"><label><?php p($l->t('Notes (optional)')); ?><input type="text" id="notes-csv" value="5"></label></div>
-				<div class="other-csv-list"><label><?php p($l->t('File contains headers')); ?><input type="checkbox" id="headers-csv" checked></label></div>
-			</div>
 			<input type="file" id="upload_csv" accept=".csv" >
 		</div>
 		<br>
