3a27a7e11753794aa4d11106fc2dcf47609719c8
Fallon Turner <fcturner@users.noreply.github.com>
2015-11-21 12:12:40 +0100

    Update part.personal.php

--- templates/part.personal.php
+++ templates/part.personal.php
@@ -24,11 +24,25 @@
 
 	<div>
 		<label>
-			<input class="check" type="checkbox" id="hide_attributes"> <?php p($l->t('Hide columns') . ': |  a-z  |  A-Z  |  0-9  |  !@#  |'); ?>
+			<input class="check" type="checkbox" id="hide_attributes"> <?php p($l->t('Hide columns') . ": '" . strtolower($l->t('Strength')) . "' & '" . strtolower($l->t('Last changed')) . "'"); ?>
 		</label>
+		<br><br>
 	</div>
 
-	<br>
+	<div>
+		<label>
+			<input class="check" type="checkbox" id="timer_bool"> <?php p($l->t('Use inactivity countdown')); ?>
+		</label>
+		<label>
+			<input type="text" id="timer" value="0"> <em id="timersettext"> <?php p($l->t('seconds')); ?> </em>
+		</label>
+		<p>
+			<?php p($l->t("This will put a timer on the lower right of the screen, which resets on activity.") . " " . $l->t("You will be logged off automatically when this countdown reaches 0")); ?>.
+		</p>
+		<p>
+			<?php p($l->t("Setting a countdown will log you off too when your session cookie ends (set to %s seconds by your administrator)", OC::$server->getConfig()->getSystemValue('session_lifetime', 60))); ?>.
+		</p>
+	</div>
 
 	<span class="msg-passwords"></span>
 
